2981de1959fd001cb75dd765a502368a96d12882
stonedhesus <ionescu_paul@outlook.com>
2022-03-30 18:45:33 +0200

    Fixed the position of the brand and I added a new animation which underlines the hovered link thus adding some additional user experience to the mix.

--- java-script/dynamic-background.js
+++ java-script/dynamic-background.js
@@ -1,16 +1,23 @@
 window.onload = function () {
-    let backgroundImg = ["\"../metadata/background/about-us/first.jpg\"",
-        "\"../metadata/background/about-us/second.jpg\"",
-        "\"../metadata/background/about-us/third.jpg\""];
 
 
-    setInterval(changeImage, 5000);
-    function changeImage() {
-        let i = Math.floor((Math.random() * 3));
+    if (/chrome/.test(navigator.userAgent.toLowerCase())){
 
-        document.getElementsByClassName("greeting-message-and-search-box").
-        item(0).setAttribute("style", "background-image: url(" + backgroundImg[i] +");");
 
+    } else {
+
+        let backgroundImg = ["\"./metadata/background/about-us/first.jpg\"",
+            "\"./metadata/background/about-us/second.jpg\"",
+            "\"./metadata/background/about-us/third.jpg\""];
+
+
+        setInterval(changeImage, 5000);
+        function changeImage() {
+            let i = Math.floor((Math.random() * 3));
+
+            console.log(backgroundImg[i]);
+            document.getElementsByClassName("greeting-message-and-search-box").
+            item(0).setAttribute("style", "background-image: url(" + backgroundImg[i] +");");
+        }
     }
 }
-
